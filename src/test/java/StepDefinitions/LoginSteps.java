4cf6eb464038fc31b52b5afade507425186e5ba2
arfaoui-aziz <mohamedaziz.arfaoui@esprit.tn>
2022-03-08 15:02:51 +0100

    fix: update cucumber version

--- src/test/java/StepDefinitions/LoginSteps.java
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -1,23 +1,53 @@
 package StepDefinitions;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class LoginSteps {
     @Given("user is on login page")
-    public void user_is_on_login_page(){
-
+    public void user_is_on_login_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @When("user gets the title of the page")
-    public void user_gets_the_title_of_the_page(){
-
+    public void user_gets_the_title_of_the_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
 
     @Then("page title should be {string}")
-    public void page_title_should_be(String pageTitle){
-
+    public void page_title_should_be(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
     }
+
+    @Then("forgot password link should be displayed")
+    public void forgot_password_link_should_be_displayed() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enters username {string}")
+    public void user_enters_username(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user enter password {string}")
+    public void user_enter_password(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("user clicks on Login Button")
+    public void user_clicks_on_login_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+
 
 }
